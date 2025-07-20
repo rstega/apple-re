362f9fa3d08c08bd031d9c238938302a042040a0
rstey <rstega780@gmail.com>
2025-07-20 09:12:20 +0300

    header

---
+++ Apple/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+// import './App.css'
+// import './component/css/styles.css';
+import{ Header} from './Header.jsx';
+function App() {
+ 
+
+  return (
+    <div>
+     <Header />
+
+    <h1>afafq;jwf;;jasfasfjas;ofaj</h1>
+
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Apple/src/Header.jsx
@@ -0,0 +1,30 @@
+
+import logo from'./component/images/icons/logo.png'
+import Search from './component/images/icons/search-icon.png';
+import Cart from'./component/images/icons/cart.png';
+ function Header() {
+  return (
+    <div>
+        <header className="header-wrapper">
+			<div className="internal-wrapper"> 
+				<div className="header-links-wrapper">
+					<ul>
+						<li className="logo-link"><a href="/"><img src={logo} /></a></li>
+						<li><a href="/mac/">Mac</a></li>
+						<li><a href="#">iphone</a></li>
+						<li><a href="#">ipad</a></li>
+						<li><a href="#">watch</a></li>
+						<li><a href="#">tv</a></li>
+						<li><a href="#">Music</a></li>
+						<li><a href="#">Support</a></li>
+						<li className="search-link"><a href="/search/"><img src={Search}/></a></li>
+						<li className="cart-link"><a  href="/cart/"><img src={Cart} /></a></li>
+					</ul>					
+				</div>
+			</div>
+		</header>
+    </div>
+  
+  )
+  }
+  export default Header; 
